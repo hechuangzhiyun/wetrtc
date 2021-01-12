c0fd1c09b9b6d1e9ee9e46692b4e8aadfc43c495
hushiyun1994 <63150499+hushiyun1994@users.noreply.github.com>
2021-01-12 21:14:01 +0800

    123

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const express = require('express')
 const app = express()
+const fs = require('fs')
+const https = require('https')
 
 app.listen(3000)
 
@@ -7,17 +9,26 @@ app.use(express.static('public'))
 
 const WebSocket = require('ws');
 
-const wss = new WebSocket.Server({
-    port: 8080
+
+const server = https.createServer({
+  cert: fs.readFileSync('./cert/cert1.pem'),
+  key: fs.readFileSync('./cert/privkey1.pem')
 });
+const wss = new WebSocket.Server({ server });
+
+server.listen(8080);
 
 wss.on('listening', () => {
     console.log('listening');
 })
 
+let connections = []
+
 wss.on('connection', function connection(ws) {
+    connections.push(ws)
     ws.on('message', function incoming(message) {
-        ws.send(message)
+        connections.forEach((item)=>{
+            item.send(message)
+        })
     });
 });
-
